da2d886dce7738ca179947664facbfbe0ce9c098
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-08-05 06:21:36 -0400

    Add Styles

--- src/index.js
+++ src/index.js
@@ -13,16 +13,30 @@ const scoreInput = document.getElementById('score-input');
 
 const scores = new Request();
 
-function ShowOne(object) {
+function ShowOne(object, placenumber) {
   const div = document.createElement('div');
-  div.textContent = `${object.user}: ${object.score}`;
+  const showname = document.createElement('h3');
+  const shownumber = document.createElement('h3');
+  const place = document.createElement('h3');
+
+  showname.textContent = object.user;
+  shownumber.textContent = object.score;
+  place.textContent =  `#${placenumber}`;
   div.classList.add('score');
+  showname.classList.add('showname');
+  shownumber.classList.add('shownumber');
+  place.classList.add('place');
+  div.appendChild(place);
+  div.appendChild(showname);
+  div.appendChild(shownumber);
   list.appendChild(div);
 }
 
 function showAll(objectList) {
+  let place = 0;
   objectList.forEach((object) => {
-    ShowOne(object);
+    place += 1;
+    ShowOne(object, place);
   });
 }
 
